cab109037d0a4f87529b7a7e8638666da7f19b98
Dmitry Murzo <dmitrymurzo1@gmail.com>
2022-10-18 11:11:16 +0300

    initial commit

---
+++ src/test/java/by/murzo/meetup/MeetupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.murzo.meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
